64fba7061cf807bb1dc4ec2324d9c3dfb1bd0ef0
Anurag <anurag.131092@gmail.com>
2023-09-07 14:08:05 +0530

    HOME, footer complete

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,29 @@
 <footer>
-  By&nbsp;<a href="http://envisagecyberart.in" target="_blank">Anurag Srivastava</a>
+  <div class="content container mx-auto p-12">
+    <hr class="mb-12">
+    <div class="flex flex-row h-full w-full items-stretch justify-center text-left">
+      <div class="border border-y-0 border-l-0 basis-1/2">
+        <h4 class="subtitle">
+          SUBSCRIBE FOR MORE
+        </h4>
+        <form action="#" class="mt-8 w-full mt-8 w-[75%] flex flex-row items-stretch justify-start">
+          <input type="email" placeholder="Enter email here" class="grow mr-4 form-input px-4 py-3 rounded-md">
+          <button type="submit" class="px-4 py-3 rounded-md text-white bg-black hover:bg-gray-600">
+            SUBMIT
+          </button>
+        </form>
+      </div>
+      <div class="links basis-1/2 grid grid-cols-1 gap-2 text-right">
+        <div>
+          <a href="#">VIEW PORTFOLIO</a>
+        </div>
+        <div>
+          <a href="#">GET IN TOUCH</a>
+        </div>
+        <div>
+          <a href="#" class="text-[#7C7C7C]">SOFIA BORGES LLC</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
